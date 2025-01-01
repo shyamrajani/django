9bf0c94f367453f0a6d735e26b40f35f42a09018
shyamrajani <shyamr.albiorix@gmail.com>
2025-01-01 22:11:39 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+
+
+urlpatterns = [
+    path('register/' ,RegisterView.as_view()),
+    
+    path('verify-otp/' , VerifyOtp.as_view())
+]
